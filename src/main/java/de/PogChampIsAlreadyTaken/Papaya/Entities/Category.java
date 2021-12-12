3b396698698d31f4d01e15fb9a9e91289c51e748
Alois Roscher <aloisroscher@gmail.com>
2021-12-13 00:58:28 +0100

    added finder methods for menuid and menucategory. Added sql-scripts to fill the data

--- src/main/java/de/PogChampIsAlreadyTaken/Papaya/Entities/Category.java
+++ src/main/java/de/PogChampIsAlreadyTaken/Papaya/Entities/Category.java
@@ -1,18 +1,10 @@
 package de.PogChampIsAlreadyTaken.Papaya.Entities;
-
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import java.util.List;
+
 
 
 @Entity
 public class Category extends PanacheEntity {
-
     public String name;
-
-    @OneToMany
-    public List<Meal> mealsList;
-
 }
